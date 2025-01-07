43ad8cd9b02f5ded38a8f8526f7051dc3985b7a9
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2025-01-07 20:55:49 -0300

    - remove extensions

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,8 +1,126 @@
 import { DateRangePicker } from '../DateRangePicker';
+import { Accordion } from './accordion';
 import { Alert } from './alert';
+import { AlertDialog } from './alert-dialog';
+import { AspectRatio } from './aspect-ratio';
+import { Avatar } from './avatar';
 import { Badge } from './badge';
+import { Breadcrumb } from './breadcrumb';
 import { Button } from './button';
+import { Calendar } from './calendar';
+import { Card } from './card';
+import { Carousel } from './carousel';
 import { Checkbox } from './checkbox';
+import { Collapsible } from './collapsible';
+import { Command } from './command';
+import { ContextMenu } from './context-menu';
+import { DateTimePickerForm } from './date-time-picker-form';
+import { Dialog } from './dialog';
+import { Drawer } from './drawer';
+import { DropdownMenu } from './dropdown-menu';
+import { Form } from './form';
+import { HoverCard } from './hover-card';
 import { Input } from './input';
+import { InputOTP } from './input-otp';
+import { Label } from './label';
+import { Menubar } from './menubar';
+import { NavigationMenu } from './navigation-menu';
+import { Pagination } from './pagination';
+import { Popover } from './popover';
+import { Progress } from './progress';
+import { RadioGroup } from './radio-group';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from './resizable';
+import { ScrollArea } from './scroll-area';
+import { Select } from './select';
+import { Separator } from './separator';
+import { Sheet } from './sheet';
+import { Sidebar } from './sidebar';
+import { Skeleton } from './skeleton';
+import { Slider } from './slider';
+import { Switch } from './switch';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './table';
+import { Tabs } from './tabs';
+import { Textarea } from './textarea';
+import { TimePickerDemo } from './time-picker-demo';
+import { TimePickerInput } from './time-picker-input';
+import { Toast } from './toast';
+import { Toaster } from './toaster';
+import { Toggle } from './toggle';
+import { ToggleGroup } from './toggle-group';
+import { Tooltip } from './tooltip';
 
-export { Alert, Checkbox, Button, Input, DateRangePicker, Badge };
+export {
+  Alert,
+  Checkbox,
+  Button,
+  Input,
+  DateRangePicker,
+  Badge,
+  Accordion,
+  AlertDialog,
+  AspectRatio,
+  Avatar,
+  Breadcrumb,
+  Calendar,
+  Card,
+  Carousel,
+  Chart,
+  Collapsible,
+  Command,
+  ContextMenu,
+  DateTimePickerForm,
+  Dialog,
+  Drawer,
+  DropdownMenu,
+  Form,
+  HoverCard,
+  InputOTP,
+  Label,
+  Menubar,
+  NavigationMenu,
+  Pagination,
+  Popover,
+  Progress,
+  RadioGroup,
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+  ScrollArea,
+  Select,
+  Separator,
+  Sheet,
+  Sidebar,
+  Skeleton,
+  Slider,
+  Switch,
+  Table,
+  TableHeader,
+  TableBody,
+  TableFooter,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableCaption,
+  Tabs,
+  Textarea,
+  TimePickerDemo,
+  TimePickerInput,
+  Toast,
+  Toaster,
+  ToggleGroup,
+  Toggle,
+  Tooltip,
+};
