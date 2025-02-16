2d61f3edaff5fa47e3f7f05d46d298977b07a426
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2025-02-16 01:57:34 -0300

    - removing unused imports

--- src/ac-components/components/Home.tsx
+++ src/ac-components/components/Home.tsx
@@ -1,12 +1,6 @@
 import { DailyHabitList } from '@/ac-components/components/DailyHabitList';
 import { DayNavigation } from '@/ac-components/components/DayNavigation';
 import { HabitForm } from '@/ac-components/components/HabitFormComponent';
-import {
-  getHabitsFromStorage,
-  saveHabitsToStorage,
-} from '@/ac-components/lib/habits';
-import { pluginManager } from '@/ac-components/lib/plugins';
-import type { Habit } from '@/ac-components/types/habits';
 import { useNavigate } from '@modern-js/runtime/router';
 import { Calendar, Home as HomeIcon, PlusCircle, Settings } from 'lucide-react';
 import { useEffect, useState } from 'react';
