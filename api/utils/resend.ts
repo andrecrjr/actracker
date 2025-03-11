743accfe2b0d3f7d46ccb48db717d3818b1c760f
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2025-03-11 01:26:04 -0300

    - fix resend api obligation

--- api/utils/resend.ts
+++ api/utils/resend.ts
@@ -1,7 +1,7 @@
 // utils/email.ts
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export const sendEmail = async (
   to: string,
